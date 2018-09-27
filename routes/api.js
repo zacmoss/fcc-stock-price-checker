5367e2fc302343b17540065aa4b1169e80afa129
Glitch (terrific-sky) <none>
2018-09-27 21:53:01 +0000

    👂😭 Checkpoint ./routes/api.js:1744758/416

--- routes/api.js
+++ routes/api.js
@@ -19,8 +19,12 @@ module.exports = function (app) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       let stock = req.query.stock;
+      let like;
+      console.log(stock);
       if (req.query.like) {
-        console.log('like');
+        if (req.query.like == 'true') {
+          like = true;
+        }
       }
       let storedData = {
         ticker: '',
@@ -29,7 +33,7 @@ module.exports = function (app) {
       };
       
       let stockData = {};
-    
+      console.log(like);
     // companyName   symbol    
     axios.get('https://api.iextrading.com/1.0/stock/aapl/batch?types=quote,news,chart&range=1m&last=1').then(json => {
       res.send(json.data.quote);
