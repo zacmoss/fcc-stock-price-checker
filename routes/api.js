2e83e088129bfdf11cdf2aee709ff63201ce72f3
Glitch (terrific-sky) <none>
2018-09-27 18:53:54 +0000

    📺🌝 Checkpoint ./routes/api.js:1744758/567 ./package.json:1744758/26

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,7 @@
 
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
+const axios = require('axios');
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
@@ -17,7 +18,18 @@ module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      let stock = req.query.stock;
+    /*
+      app.get('https://finance.google.com/finance/info?q=NASDAQ%3a' + stock, function(req, res) {
+        console.log(req);
+        console.log(res);
+      });
+      */
+    axios.get('https://finance.google.com/finance/info?q=NASDAQ%3a' + stock).then(json => {
+      console.log(json);
+    }).catch(error => {
+      console.log(error);
+    });
     });
     
 };
